6e642f627a88143f03c19a52851beae2688d341d
VayneIrisu <vayneirisu@gmail.com>
2018-10-09 18:23:26 +0700

    First Upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Japan's PTPN XI
                 </div>
 
                 <div class="links">
